7e259158dedeb199d1275a25b1c421a27133fa7e
sfdevelop <sfdevelop747@gmail.com>
2020-12-10 22:24:56 +0200

    10-12

--- assets/js/main.js
+++ assets/js/main.js
@@ -3063,3 +3063,17 @@ $(document).ready(function () {
 		]
 	});
 });
+
+//боковое  меню
+$(document).ready(function () {
+	$("#navToggle").click(function () {
+		$(this).toggleClass("active");
+		$(".overlay").toggleClass("open");
+		// this line ▼ prevents content scroll-behind
+		$("body").toggleClass("locked");
+	});
+	$('.mobile_li').click(function (ev) {
+		$(this).find('>ul').slideToggle();
+		ev.stopPropagation();
+	});
+});
